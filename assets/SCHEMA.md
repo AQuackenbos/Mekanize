a8ae29821dbdce1d4dfffc8b365fd26c52faa08c
Andrew Quackenbos <andrew@sitesquad.net>
2013-10-06 08:02:06 -0400

    Schema update, initial test map

--- assets/SCHEMA.md
+++ assets/SCHEMA.md
@@ -1,11 +1,43 @@
 Data Schemas
 ========
 
+## Type
+
+
+
 ## Meks
 
 	mek:
 	{
-		id: 0, //ID: unique numerical id of the mek type
+		id: 0, //player mek id
+		body: 0, //Body ID
+		hdd: 0, //player HDD id
+		item: 0, //player inventory item id
+	}
+
+## Bodies
+
+	body:
+	{
+		id: 0, //ID: unique numerical id of the body type
+		core: [{
+			id: 0, //Core type ID
+		}], 
+		parts: //Parts array required
+		[{
+			id: 0, //Item type ID
+			qty: 0, //Qty required or given
+		},],
+		stats: //base stat data
+		{
+			hull: 0,
+			force: 0,
+			armor: 0,
+			systems: 0,
+			circuitry: 0,
+			maneuver: 0,
+			agility: 0,
+		},
 	}
 
 ## Items
@@ -13,9 +45,59 @@ Data Schemas
 	item:
 	{
 		id: 0, //ID: unique numerical id of the item type
+		name: "Item Name", //Frontend name for item
+		requires: //optional as well as each subfield
+		{
+			level: 0, //Level of HDD required to use item
+			ability: 0, //ID of ability required to use item
+			body: 0, //ID of body type required to use item
+		}
 	}
 
+### Cores
+Cores extend off the basic item schema with more information.  They should never have any requirements.  
+
+	core: 
+	{
+		vs:
+		{
+			0: 1.0//opposing type ID:received damage multiplier
+		},
+		terrain:
+		{
+			0: //terrain type ID
+			{
+				power: 1.5 //stat: multiplier
+			},
+		},
+	}
+	
+### Player Item
+
+	player_item:
+	{
+		id: 0, //player item id
+		item: 0 //item type id
+	}
+	
 ## Harddrives
+
+	hdd:
+	{
+		id: 0, //player hdd id
+		body: 0, //required body ID (set when hdd created)
+	}
 
 ## Programming
 
+    program:
+	{
+	
+	}
+	
+### Moves
+	
+	move:
+	{
+	
+	}
--- src/mekanize.js
+++ src/mekanize.js
@@ -1,8 +1,110 @@
-Game = {
-  // Initialize and start our game
-  start: function() {
-    // Start crafty and set a background color so that we can see it's working
-    Crafty.init(800,600);
-    Crafty.background('black');
-  }
-}
+window.Mekanize = {
+	config: {
+		SPRITE: 
+		{
+			SIZE: 128
+		},
+		MAP: 
+		{
+			TILESIZE: 128,
+			WIDTH: 25,
+			HEIGHT: 25
+		}
+	},
+};
+
+window.MAP = {
+	2: {
+		3: true,
+		4: true,
+		5: true,
+	},
+	3: {
+		1: true,
+		2: true,
+		3: true,
+		4: true,
+		5: true,
+		6: true,
+		7: true
+	},
+	4: {
+		0: true,
+		1: true,
+		2: true,
+		3: true,
+		4: true,
+		5: true,
+		6: true,
+		7: true,
+		8: true
+	},
+	5: {
+		2: true,
+		3: true,
+		4: true,
+		5: true,
+		6: true
+	},
+	6: {
+		4: true
+	}
+};
+
+$(document).ready(function() {
+	Crafty.init();
+
+	Crafty.sprite(Mekanize.config.SPRITE.SIZE, "assets/test.png", {
+		grass: [0,0,1,1],
+		stone: [1,0,1,1]
+	});
+
+	var iso = Crafty.isometric.size(Mekanize.config.MAP.TILESIZE);
+	var mapFlags = window.MAP;
+	var z = 0;
+	for(var x = Mekanize.config.MAP.WIDTH -1; x >= 0; x--) {
+		for(var y = 0; y < Mekanize.config.MAP.HEIGHT; y++) {
+			if(!mapFlags[x] || !mapFlags[x][y])
+				continue;
+		
+			var tile = Crafty.e("2D, Canvas, grass, Mouse")
+			.attr({
+				'tilex':x,
+				'tiley':y,
+				'z':x+1 * y+1
+			})
+			.bind("MouseUp", function(e) {
+				//console.log('X:'+this.tilex+' / Y:'+this.tiley);
+				//destroy on right click
+				if(e.button === 2) this.destroy();
+			})
+			.bind("MouseOver", function() {
+				this.sprite(0,1,1,1);
+			})
+			.bind("MouseOut", function() {
+				this.sprite(0,0,1,1);
+			});
+			
+			
+			iso.place(x,y,0, tile);
+		}
+	}
+	
+	Crafty.addEvent(this, Crafty.stage.elem, "mousedown", function(e) {
+		if(e.button > 1) return;
+		var base = {x: e.clientX, y: e.clientY};
+
+		function scroll(e) {
+			var dx = base.x - e.clientX,
+				dy = base.y - e.clientY;
+				base = {x: e.clientX, y: e.clientY};
+			Crafty.viewport.x -= dx;
+			Crafty.viewport.y -= dy;
+		};
+
+		Crafty.addEvent(this, Crafty.stage.elem, "mousemove", scroll);
+		Crafty.addEvent(this, Crafty.stage.elem, "mouseup", function() {
+			Crafty.removeEvent(this, Crafty.stage.elem, "mousemove", scroll);
+		});
+	});
+});
